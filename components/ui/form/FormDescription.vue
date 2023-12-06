66bef16f42cf713bffe3f86412ddc9e622536c17
Hypnos <47209391+Jace254@users.noreply.github.com>
2023-12-06 07:49:47 +0300

    feat: add form ui component for shadcn

---
+++ components/ui/form/FormDescription.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { useAttrs } from 'vue'
+import { useFormField } from './useFormField'
+import { cn } from '@/utils'
+
+defineOptions({
+  inheritAttrs: false,
+})
+
+const { formDescriptionId } = useFormField()
+const { class: className, ...rest } = useAttrs()
+</script>
+
+<template>
+  <p
+    :id="formDescriptionId"
+    :class="cn('text-sm text-neutral-500 dark:text-neutral-400', className ?? '')"
+    v-bind="rest"
+  >
+    <slot />
+  </p>
+</template>
